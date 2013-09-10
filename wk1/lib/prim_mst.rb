0b90672d2cab0d65354c823f901a51f3f2d49875
Patrick Slattery <slattery.patrickj@gmail.com>
2013-09-10 15:14:12 -0700

    make naive prims work

--- wk1/lib/prim_mst.rb
+++ wk1/lib/prim_mst.rb
@@ -6,5 +6,37 @@ def process_edges(edges_filename)
   return num_nodes, num_edges, edges
 end
 
+def sort_edges_by_weight(edges)
+  edges.clone.sort do |edge1, edge2|
+    edge_1_weight, edge_2_weight = edge1[2], edge2[2]
+    edge_1_weight <=> edge_2_weight
+  end
+end
+
+def get_vertices(edges)
+  edges.map do |edge|
+    edge[0..1]
+  end.flatten.uniq
+end
+
+def prim_mst(edges)
+  vertices = get_vertices(edges)
+  visited = [vertices.sample]
+  sorted_edges = sort_edges_by_weight(edges)
+  mst = []
+  until visited == vertices
+    # find the first edge (least weight) that crosses the frontier
+    next_edge = sorted_edges.find do |edge|
+      visited.include?(edge[0]) && !visited.include?(edge[1]) ||
+      visited.include?(edge[1]) && !visited.include?(edge[0])
+    end
+    return mst unless next_edge
+    # add the unvisited edge
+    visited << next_edge.find { |vertex| !visited.include?(vertex) }
+    mst << next_edge
+    sorted_edges.delete(next_edge)
+  end
+end
+
 num_nodes, num_edges, edges = process_edges("lib/edges.txt")
 
--- wk1/spec/prim_mst_spec.rb
+++ wk1/spec/prim_mst_spec.rb
@@ -1,11 +1,27 @@
 require 'rspec'
 require_relative '../lib/prim_mst'
 
-describe "process_edges" do
-  it "takes a filename and returns number of nodes, number of edges, and the edges" do
-    num_nodes, num_edges, edges = process_edges("lib/edges.txt")
-    num_nodes.should == 500
-    num_edges.should == 2184
-    edges.length.should == num_edges
+describe "Prim's MST Algorithm" do
+  num_vertices, num_edges, edges = process_edges("lib/edges.txt")
+
+  describe "process_edges" do
+    it "takes a filename and returns number of nodes, number of edges, and the edges" do
+      num_vertices.should == 500
+      num_edges.should == 2184
+      edges.length.should == num_edges
+    end
+  end
+
+  describe "get_vertices" do
+    it "returns the vertices of a set of edges" do
+      get_vertices(edges).length.should == num_vertices
+    end
+  end
+
+  describe "prim_mst" do
+    it "finds the mst of a small set of edges from lecture" do
+      edges = [[1, 2, 1], [1, 3, 4], [1, 4, 3], [2, 4, 2], [3, 4, 5]]
+      sort_edges_by_weight(prim_mst(edges)).should == [[1, 2, 1], [2, 4, 2], [1, 3, 4]]
+    end
   end
 end
